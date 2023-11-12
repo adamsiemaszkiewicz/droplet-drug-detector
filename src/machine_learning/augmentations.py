5fe26cdfdfb5e20218693755cb5d57322c76989c
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-12 18:52:56 +0100

    Improve augmentations module

---
+++ src/machine_learning/augmentations.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Dict, List, Optional, Type
+
+from kornia.augmentation import (
+    AugmentationBase2D,
+    ColorJitter,
+    RandomAffine,
+    RandomBrightness,
+    RandomContrast,
+    RandomCrop,
+    RandomElasticTransform,
+    RandomErasing,
+    RandomGamma,
+    RandomGaussianNoise,
+    RandomHorizontalFlip,
+    RandomPerspective,
+    RandomResizedCrop,
+    RandomRotation,
+    RandomSaturation,
+    RandomVerticalFlip,
+)
+from pydantic import BaseModel, validator
+from torch.nn import Sequential
+
+from src.common.utils.logger import get_logger
+
+_logger = get_logger(__name__)
+
+INTENSITY_AUGMENTATIONS: Dict[str, Type[AugmentationBase2D]] = {
+    "color_jitter": ColorJitter,
+    "random_brightness": RandomBrightness,
+    "random_contrast": RandomContrast,
+    "random_gamma": RandomGamma,
+    "random_gaussian_noise": RandomGaussianNoise,
+    "random_saturation": RandomSaturation,
+}
+
+GEOMETRIC_AUGMENTATIONS: Dict[str, Type[AugmentationBase2D]] = {
+    "random_affine": RandomAffine,
+    "random_crop": RandomCrop,
+    "random_elastic_transform": RandomElasticTransform,
+    "random_erasing": RandomErasing,
+    "random_horizontal_flip": RandomHorizontalFlip,
+    "random_vertical_flip": RandomVerticalFlip,
+    "random_perspective": RandomPerspective,
+    "random_rotation": RandomRotation,
+    "random_resized_crop": RandomResizedCrop,
+}
+
+ALL_AUGMENTATIONS: Dict[str, Type[AugmentationBase2D]] = {**INTENSITY_AUGMENTATIONS, **GEOMETRIC_AUGMENTATIONS}
+
+
+class AugmentationsConfig(BaseModel):
+    name_list: List[str]
+    extra_arguments_list: Optional[List[Dict[str, Any]]] = None
+
+    @validator(pre=True)
+    def fill_empty_extra_arguments_list(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        name_list, extra_arguments_list = values.get("name_list"), values.get("extra_arguments_list")
+        if extra_arguments_list is None and name_list is not None:
+            extra_arguments_list = [{} for _ in name_list]
+        values["extra_arguments_list"] = extra_arguments_list
+        return values
+
+    @validator
+    def validate_list_lengths(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        name_list, extra_arguments_list = values.get("name_list"), values.get("extra_arguments_list")
+        if name_list is not None and extra_arguments_list is not None:
+            if len(name_list) != len(extra_arguments_list):
+                raise ValueError("The length of 'name_list' and 'extra_arguments_list' must be the same.")
+        return values
+
+    @validator("name_list", each_item=True)
+    def validate_names(cls, v: str) -> str:
+        if v not in ALL_AUGMENTATIONS:
+            raise ValueError(
+                f"Augmentation '{v}' is not implemented. Available augmentations: {list(ALL_AUGMENTATIONS.keys())}"
+            )
+        return v
+
+
+def create_augmentation(augmentation_class: Type[AugmentationBase2D], arguments: Dict[str, Any]) -> AugmentationBase2D:
+    try:
+        augmentation = augmentation_class(**arguments)
+    except TypeError as e:
+        raise ValueError(f"Incorrect arguments for {augmentation_class.__name__}: {e}")
+    _logger.info(f"Created {augmentation_class.__name__} with arguments: {arguments}")
+    return augmentation
+
+
+def create_augmentations(config: AugmentationsConfig) -> Sequential:
+    if config.extra_arguments_list is None:
+        raise ValueError("'extra_arguments_list' cannot be None")
+
+    augmentations = [
+        create_augmentation(augmentation_class=ALL_AUGMENTATIONS[name], arguments=arguments)
+        for name, arguments in zip(config.name_list, config.extra_arguments_list)
+    ]
+    _logger.info(f"Augmentation sequence created with {len(augmentations)} augmentations.")
+    return Sequential(*augmentations)
