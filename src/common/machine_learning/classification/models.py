12eac29d56ff006ea144bfb010e1ac886203594f
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-12 10:42:06 +0100

    Remove files

--- src/common/machine_learning/classification/models.py
+++ src/common/machine_learning/classification/models.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from typing import Dict, List
+
 import timm
 from pydantic import BaseModel
 from torch.nn import Module
@@ -7,47 +9,47 @@ from src.common.utils.logger import get_logger
 
 _logger = get_logger(__name__)
 
+CLASSIFICATION_MODELS: List[str] = timm.list_models()
 
-class ModelConfig(BaseModel):
-    """Configuration schema to instantiate models from timm."""
+AVAILABLE_TIMM_MODELS: Dict[str, List[str]] = {
+    "classification": CLASSIFICATION_MODELS,
+}
 
-    model_name: str
+
+class ClassificationModelConfig(BaseModel):
+    """Configuration for creating a timm model."""
+
+    name: str
     pretrained: bool
     num_classes: int
     in_channels: int
 
 
-def create_model(config: ModelConfig) -> Module:
+def create_model(config: ClassificationModelConfig) -> Module:
     """
     Create a timm model based on a configuration instance.
 
     List of available architectures: https://huggingface.co/timm
 
     Args:
-        config (ModelConfig): Configuration object containing model parameters.
+        config (ClassificationModelConfig): Configuration object containing model parameters.
 
     Returns:
         Module: The created model.
 
     Raises:
-            NotImplementedError: If the model_name is not recognized.
+        NotImplementedError: If the model_name is not recognized.
     """
-    _logger.info(
-        f"Creating {'pretrained ' if config.pretrained else ''}model {config.model_name} "
-        f"with {config.num_classes} classes & {config.in_channels} input channels."
-    )
-
-    available_architectures = timm.list_models()
-
-    if config.model_name not in available_architectures:
+    if config.name not in AVAILABLE_TIMM_MODELS:
         raise NotImplementedError(
-            f"Model {config.model_name} not found in timm. Check available architectures at https://huggingface.co/timm"
+            f"Model '{config.name}' not found in timm. Check available architectures at https://huggingface.co/timm"
         )
 
     model = timm.create_model(
-        model_name=config.model_name,
+        model_name=config.name,
         pretrained=config.pretrained,
         num_classes=config.num_classes,
         in_chans=config.in_channels,
     )
+
     return model
