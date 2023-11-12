ccf0eae9a52ff673023cd6aec2af3527b052f64a
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-12 17:20:40 +0100

    Improve lightning module

---
+++ src/machine_learning/classification/lightning_module.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+from typing import Dict, Optional, Tuple, Union
+
+from lightning import LightningModule
+from torch import Tensor
+from torch.nn import Module
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
+
+from src.common.consts.machine_learning import STAGE_TESTING, STAGE_TRAINING, STAGE_VALIDATION
+from src.machine_learning.classification.config import ClassificationConfig
+from src.machine_learning.classification.loss_functions import create_loss_function
+from src.machine_learning.classification.metrics import create_metrics
+from src.machine_learning.classification.models import create_model
+from src.machine_learning.optimizer import create_optimizer
+from src.machine_learning.scheduler import create_scheduler
+
+
+class ClassificationLightningModule(LightningModule):
+    """
+    PyTorch Lightning module for a classification task.
+
+    This class extends LightningModule and is configured via a ClassificationConfig object.
+    It defines the forward pass, training step, validation step, and test step, as well as
+    the optimizer and learning rate scheduler setup.
+
+    Attributes:
+        config (ClassificationConfig): A configuration object
+        model (Module): The neural network model defined by the create_model function.
+        loss_function (Module): The loss function, instantiated based on config.
+        metrics (Dict[str, Metric]): A dictionary mapping metric names to Metric instances.
+        augmenter (Optional[Module]): An optional data augmentation module.
+    """
+
+    def __init__(self, config: ClassificationConfig, augmenter: Optional[Module] = None):
+        super().__init__()
+        self.config = config
+        self.model = create_model(config=self.config.model)
+        self.loss_function = create_loss_function(config=self.config.loss_function)
+        self.metrics = create_metrics(config=self.config.metrics)
+
+        self.augmenter = augmenter
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.model(x)
+
+    def evaluation_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int, stage: str) -> Dict[str, Tensor]:
+        """
+        Shared evaluation step for validation and testing.
+
+        Args:
+            batch (Tuple[Tensor, Tensor]): The current batch of inputs, labels & reference GeoTIFFs.
+            batch_idx (int): Index of the current batch.
+            stage (str): Current stage (e.g., validation or testing).
+
+        Returns:
+            Dict[str, Tensor]: Loss & predictions for the current batch.
+        """
+        x, y = batch
+
+        logits = self(x)
+        loss = self.compute_loss(logits=logits, targets=y)
+        preds = logits.argmax(dim=1)
+
+        self.log(name=f"{stage}_loss", value=loss)
+        self.log_metrics(logits=logits, targets=y, stage=stage)
+
+        return {"loss": loss, "preds": preds}
+
+    def training_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int) -> Dict[str, Tensor]:
+        """
+        Defines the procedure of a single training step.
+
+        Args:
+            batch (Tuple[Tensor, Tensor): The current batch of training data, labels & reference GeoTIFFs.
+            batch_idx (int): Index of the current batch.
+
+        Returns:
+            Dict[str, Tensor]: Training loss.
+        """
+        return self.evaluation_step(batch=batch, batch_idx=batch_idx, stage=STAGE_TRAINING)
+
+    def validation_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int) -> Dict[str, Tensor]:
+        """
+        Defines the procedure of a single validation step.
+
+        Args:
+            batch (Tuple[Tensor, Tensor]): The current batch of validation data and labels.
+            batch_idx (int): Index of the current batch.
+
+        Returns:
+            Dict[str, Tensor]: Validation loss.
+        """
+        return self.evaluation_step(batch=batch, batch_idx=batch_idx, stage=STAGE_VALIDATION)
+
+    def test_step(self, batch: Tuple[Tensor, Tensor], batch_idx: int) -> Dict[str, Tensor]:
+        """
+        Defines the procedure of a single testing step.
+
+        Args:
+            batch (Tuple[Tensor, Tensor]): The current batch of testing data and labels.
+            batch_idx (int): Index of the current batch.
+
+        Returns:
+            Dict[str, Tensor]: Testing loss.
+        """
+        return self.evaluation_step(batch=batch, batch_idx=batch_idx, stage=STAGE_TESTING)
+
+    def on_after_batch_transfer(self, batch: Tuple[Tensor, Tensor], dataloader_idx: int) -> Tuple[Tensor, Tensor]:
+        """
+        Hook to perform operations on the batch after the batch transfer but before passing to the model.
+
+        If the model is in training mode and an augmenter is provided, it will apply augmentation
+        to the batch of data.
+
+        Args:
+            batch (Tuple[Tensor, Tensor]): A tuple containing a batch of input data and labels.
+            dataloader_idx (int): The index of the dataloader that provided the batch.
+
+        Returns:
+            Tuple[Tensor, Tensor]: A tuple of augmented data and labels, or the original batch
+            if no augmentation is to be applied.
+        """
+        x, y = batch
+        if self.trainer.training and self.augmenter:
+            x = self.augmenter(x)
+        return x, y
+
+    def configure_optimizers(self) -> Dict[str, Union[Optimizer, LRScheduler]]:
+        """
+        Configure the optimizer using the model parameters and specified learning rate.
+
+        Returns:
+            Dict[str, Union[Optimizer, LRScheduler]]: Optimizer and optional learning rate scheduler
+        """
+        optimizer = create_optimizer(config=self.config.optimizer, parameters=self.parameters())
+        optimizers_config = {"optimizer": optimizer}
+
+        if self.config.scheduler:
+            scheduler = create_scheduler(config=self.config.scheduler, optimizer=optimizer)
+            optimizers_config["scheduler"] = scheduler
+
+        return optimizers_config
+
+    def compute_loss(self, logits: Tensor, targets: Tensor) -> Tensor:
+        """
+        Compute the loss based on logits and targets.
+
+        Args:
+            logits (Tensor): The model outputs before activation.
+            targets (Tensor): The true labels.
+
+        Returns:
+            Tensor: The computed loss.
+        """
+        return self.loss_function(logits, targets)
+
+    def log_metrics(self, logits: Tensor, targets: Tensor, stage: str) -> None:
+        """
+        Log metrics to the logger.
+
+        Args:
+            logits (Tensor): The model outputs before activation.
+            targets (Tensor): The true labels.
+            stage (str): The current stage (e.g., training, validation, or testing).
+        """
+        for name, metric in self.metrics.items():
+            metric_value = metric(logits, targets)
+            self.log(f"{stage}_{name}", metric_value, on_step=True, on_epoch=True)
