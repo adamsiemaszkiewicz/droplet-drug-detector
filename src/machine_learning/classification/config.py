4eb95119db5e62184d18a59290002363bc8dff12
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-14 16:06:13 +0100

    Add callbacks, loggers & trainer sections

--- src/machine_learning/classification/config.py
+++ src/machine_learning/classification/config.py
@@ -4,11 +4,14 @@ from typing import Optional
 from pydantic import BaseSettings
 
 from src.machine_learning.augmentations import AugmentationsConfig
+from src.machine_learning.callbacks import CallbacksConfig
 from src.machine_learning.classification.loss_functions import ClassificationLossFunctionConfig
 from src.machine_learning.classification.metrics import ClassificationMetricsConfig
 from src.machine_learning.classification.models import ClassificationModelConfig
+from src.machine_learning.loggers import LoggersConfig
 from src.machine_learning.optimizer import OptimizerConfig
 from src.machine_learning.scheduler import SchedulerConfig
+from src.machine_learning.trainer import TrainerConfig
 
 
 class ClassificationConfig(BaseSettings):
@@ -18,3 +21,6 @@ class ClassificationConfig(BaseSettings):
     scheduler: Optional[SchedulerConfig] = None
     metrics: ClassificationMetricsConfig
     augmentations: Optional[AugmentationsConfig] = None
+    callbacks: Optional[CallbacksConfig] = None
+    loggers: Optional[LoggersConfig] = None
+    trainer: TrainerConfig
