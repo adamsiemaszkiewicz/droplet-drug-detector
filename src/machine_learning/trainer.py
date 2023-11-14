96c5d05b45dae10425c7d9ad2e24114951cc7041
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-14 19:09:06 +0100

    Refactor trainer module

--- src/machine_learning/trainer.py
+++ src/machine_learning/trainer.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Dict, List, Optional
+from typing import List, Optional, Type
 
 from lightning import Trainer
 from lightning.pytorch.callbacks import Callback
 from lightning.pytorch.loggers import Logger
-from pydantic import BaseModel, root_validator
+from pydantic import BaseModel, validator
 
 from src.common.utils.logger import get_logger
 
@@ -13,41 +13,46 @@ _logger = get_logger(__name__)
 
 class TrainerConfig(BaseModel):
     """
-    Configuration for creating a PyTorch Lightning Trainer instance.
+    Configuration for creating a PyTorch Lightning trainer.
 
-    This class uses Pydantic for data validation and settings management. Add additional fields
-    as required for your specific configuration needs.
+    Attributes:
+        max_epochs: The maximum number of epochs to train for.
+        callbacks: A list of PyTorch Lightning callbacks to use.
+        logger: A list of PyTorch Lightning loggers to use.
     """
 
-    max_epochs: int = 20
-    callbacks: Optional[list] = None  # Expect this to be a list of PyTorch Lightning Callback instances
-    logger: Optional[list] = None  # Expect this to be a list of PyTorch Lightning Logger instances
+    max_epochs: int
+    callbacks: Optional[List[Type[Callback]]] = None
+    logger: Optional[List[Type[Logger]]] = None
 
-    @root_validator(pre=True)
-    def check_gpu_availability(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+    @validator("max_epochs")
+    def validate_positive_integer(cls, v: int) -> int:
         """
-        Validates that GPUs are available if specified in the configuration.
+        Validates if the provided value is a positive integer.
         """
-        gpus = values.get("gpus", None)
-        if gpus:
-            # Here you would typically check if the specified number of GPUs are available.
-            # For example, using torch.cuda.is_available() and torch.cuda.device_count()
-            pass  # Replace with actual validation logic
-        return values
+        if not isinstance(v, int) or v <= 0:
+            raise ValueError(f"The value {v} must be a positive integer.")
+        return v
 
 
-def create_trainer(config: TrainerConfig, callbacks: List[Callback], loggers: List[Logger]) -> Trainer:
+def create_trainer(
+    config: TrainerConfig, callbacks: Optional[List[Callback]], loggers: Optional[List[Logger]]
+) -> Trainer:
     """
-    Creates a PyTorch Lightning Trainer instance from a given configuration.
+    Create a PyTorch Lightning Trainer instance from a given configuration.
 
     Args:
         config (TrainerConfig): The configuration object for the Trainer.
-        callbacks (List[Callback]): A list of PyTorch Lightning callback to use.
-        loggers (List[Logger]): A list of PyTorch Lightning loggers to use.
+        callbacks (Optional[List[Callback]]): A list of PyTorch Lightning callback to use.
+        loggers (Optional[List[Logger]]): A list of PyTorch Lightning loggers to use.
 
     Returns:
         Trainer: An instance of the PyTorch Lightning Trainer configured as per the provided settings.
     """
+    _logger.info(f"Creating trainer with the following configuration: {config.dict()}")
+
     trainer = Trainer(max_epochs=config.max_epochs, logger=loggers, callbacks=callbacks)
+
+    _logger.info("Trainer successfully created.")
 
     return trainer
