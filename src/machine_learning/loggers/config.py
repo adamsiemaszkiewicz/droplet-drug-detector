257a1e6fec34c6f22fee7e17ea479ed8d3f57bf5
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-21 17:41:22 +0100

    Split loggers module into smaller pieces

--- src/machine_learning/loggers/config.py
+++ src/machine_learning/loggers/config.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Type, Union
+from typing import Any, Dict, List, Optional, Union
 
-from lightning.pytorch.loggers import CSVLogger, Logger, MLFlowLogger, TensorBoardLogger
 from pydantic import validator
 
 from src.common.utils.logger import get_logger
 from src.configs.base import BaseLoggersConfig
+from src.machine_learning.loggers.types import AVAILABLE_LOGGERS
 
 _logger = get_logger(__name__)
-
-AVAILABLE_LOGGERS: Dict[str, Type[Logger]] = {
-    "csv": CSVLogger,
-    "mlflow": MLFlowLogger,
-    "tensorboard": TensorBoardLogger,
-}
 
 
 class LoggersConfig(BaseLoggersConfig):
@@ -78,49 +72,3 @@ class LoggersConfig(BaseLoggersConfig):
         if not isinstance(v, Path):
             return Path(v)
         return v
-
-
-def create_logger(
-    logger_class: Type[Logger], save_dir: Union[str, Path], arguments: Optional[Dict[str, Any]] = None
-) -> Logger:
-    """
-    Creates a logger based on the configuration.
-
-    Args:
-        logger_class (Type[Logger]): The logger class.
-        save_dir (Union[str, Path]): The directory where the logs will be saved.
-        arguments (Optional[Dict[str, Any]]: Additional arguments specific to the metric.
-
-    Returns:
-        Logger: An instance of the specified logger class.
-    """
-    config = {"save_dir": save_dir}
-    config.update(arguments or {})
-
-    logger = logger_class(**config)
-
-    _logger.info(f"Created logger '{logger_class.__name__}' with the following configuration: {config}")
-
-    return logger
-
-
-def create_loggers(config: LoggersConfig) -> List[Logger]:
-    """
-    Creates a list of logger instances from a LoggersConfig instance.
-
-    Args:
-        config (LoggersConfig): A LoggersConfig instance.
-
-    Returns:
-        List[LightningLoggerBase]: A list of logger instances.
-    """
-    _logger.info(f"Creating {len(config.name_list)} loggers.")
-
-    loggers = [
-        create_logger(logger_class=AVAILABLE_LOGGERS[name], save_dir=config.save_dir, arguments=args)
-        for name, args in zip(config.name_list, config.extra_arguments_list)
-    ]
-
-    _logger.info("Metrics configured successfully.")
-
-    return loggers
