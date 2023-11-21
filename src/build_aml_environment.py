d055dabad2e64becb75184978ed68da35bb3bb0e
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-21 15:55:06 +0100

    Take explicit arguments instead of config object

--- src/build_aml_environment.py
+++ src/build_aml_environment.py
@@ -75,7 +75,14 @@ def main() -> None:
     config = parse_args()
     settings = Settings()
 
-    ml_client = get_ml_client(settings=settings)
+    ml_client = get_ml_client(
+        tenant_id=settings.az.tenant_id,
+        client_id=settings.aml.sp_client_id,
+        client_secret=settings.aml.sp_client_secret,
+        subscription_id=settings.az.subscription_id,
+        resource_group_name=settings.az.resource_group,
+        workspace_name=settings.aml.workspace_name,
+    )
 
     if config.sample_project:
         _build_sample_project_environment(ml_client=ml_client, runtime_env=config.runtime_env, tag=config.tag)
