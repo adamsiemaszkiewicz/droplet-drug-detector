cc2a9f4aa956b3abb08a5048fe6e8bb49cf57f1e
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-21 17:52:49 +0100

    Split trainer module int osmaller submodules

--- src/machine_learning/trainer/config.py
+++ src/machine_learning/trainer/config.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
-from typing import List, Literal, Optional, Union
+from typing import Literal, Optional, Union
 
-from lightning import Trainer
-from lightning.pytorch.callbacks import Callback
-from lightning.pytorch.loggers import Logger
 from pydantic import validator
 
 from src.common.utils.logger import get_logger
@@ -49,28 +46,3 @@ class TrainerConfig(BaseTrainerConfig):
         if not isinstance(v, int) or v <= 0:
             raise ValueError(f"The value {v} must be a positive integer.")
         return v
-
-
-def create_trainer(
-    config: TrainerConfig, callbacks: Optional[List[Callback]], loggers: Optional[List[Logger]]
-) -> Trainer:
-    """
-    Create a PyTorch Lightning Trainer instance from a given configuration.
-
-    Args:
-        config (TrainerConfig): The configuration object for the Trainer.
-        callbacks (Optional[List[Callback]]): A list of PyTorch Lightning callback to use.
-        loggers (Optional[List[Logger]]): A list of PyTorch Lightning loggers to use.
-
-    Returns:
-        Trainer: An instance of the PyTorch Lightning Trainer configured as per the provided settings.
-    """
-    args = {name: args for name, args in config.dict().items() if args is not None}
-
-    _logger.info(f"Creating trainer with the following configuration: {args}")
-
-    trainer = Trainer(logger=loggers, callbacks=callbacks, **args)
-
-    _logger.info("Trainer successfully created.")
-
-    return trainer
