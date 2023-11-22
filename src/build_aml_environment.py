eceab0b8d104a89b7a2d09940d7e6eaff446e0d5
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-22 20:57:49 +0100

    Remove tag. Use names instead of flags

--- src/build_aml_environment.py
+++ src/build_aml_environment.py
@@ -18,38 +18,31 @@ from src.common.utils.logger import get_logger
 _logger = get_logger(__name__)
 
 
-def _build_sample_project_environment(
-    ml_client: MLClient, runtime_env: Literal["dev", "prod"], tag: Optional[str] = None
-) -> Environment:
+def _build_sample_project_environment(ml_client: MLClient, runtime_env: Literal["dev", "prod"]) -> Environment:
     """
     Build or update a specific Azure ML Environment.
 
     Args:
         ml_client (MLClient): The MLClient object.
         runtime_env (Literal["dev", "prod"]): The runtime environment. Can be either "dev" or "prod".
-        tag (Optional[str]): The version tag for the environment.
 
     Returns:
         Environment: The built or updated Azure ML Environment.
     """
-    name = "sample_project"
-    env_name = f"{runtime_env}_{name}_env"
+    name = "sample-project"
+    env_name = f"{runtime_env}-{name}-env"
     conda_dependencies_file_path = ENVIRONMENTS_DIR / f"{name}{YAML}"
 
     _logger.info(f"Building environment: {env_name}")
 
     return build_environment(
-        ml_client=ml_client,
-        name=env_name,
-        enable_gpu=False,
-        conda_dependencies_file_path=conda_dependencies_file_path,
-        tag=tag,
+        ml_client=ml_client, name=env_name, enable_gpu=False, conda_dependencies_file_path=conda_dependencies_file_path
     )
 
 
 class EnvironmentBuildingConfig(BaseModel):
     runtime_env: Literal["dev", "prod"]
-    sample_project: bool
+    environment_name: str
     tag: Optional[str] = None
 
     def __str__(self) -> str:
@@ -59,7 +52,7 @@ class EnvironmentBuildingConfig(BaseModel):
 def parse_args() -> EnvironmentBuildingConfig:
     parser = argparse.ArgumentParser()
     parser.add_argument("--runtime_env", type=str)
-    parser.add_argument("--sample_project", action="store_true")
+    parser.add_argument("--environment_name", type=str)
     parser.add_argument("--tag", type=str)
 
     args = parser.parse_args()
@@ -84,8 +77,8 @@ def main() -> None:
         workspace_name=settings.aml.workspace_name,
     )
 
-    if config.sample_project:
-        _build_sample_project_environment(ml_client=ml_client, runtime_env=config.runtime_env, tag=config.tag)
+    if config.environment_name == "sample-project":
+        _build_sample_project_environment(ml_client=ml_client, runtime_env=config.runtime_env)
     else:
         raise NotImplementedError(f"Unknown environment specified ({config.runtime_env}).")
 
