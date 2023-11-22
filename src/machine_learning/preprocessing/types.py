50c4652e3f02267c55c1a4d7cdff4757ad4447b9
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-22 22:44:56 +0100

    Add padding

--- src/machine_learning/preprocessing/types.py
+++ src/machine_learning/preprocessing/types.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import Dict, List, Type, Union
 
-from kornia.augmentation import AugmentationBase2D, CenterCrop, Normalize, Resize
+from kornia.augmentation import AugmentationBase2D, CenterCrop, Normalize, PadTo, Resize
 
 from src.common.utils.logger import get_logger
 
@@ -10,10 +10,12 @@ _logger = get_logger(__name__)
 AVAILABLE_TRANSFORMATIONS: Dict[str, Type[AugmentationBase2D]] = {
     "center_crop": CenterCrop,
     "normalize": Normalize,
+    "pad": PadTo,
     "resize": Resize,
 }
 
 REQUIRED_ARGUMENTS: Dict[str, Union[str, List[str]]] = {
     "normalize": ["mean", "std"],
+    "pad": "size",
     "resize": "size",
 }
