d860290186d8564100e54bca053efbc2293aa974
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-24 16:38:46 +0100

    Replace config class

--- src/aml/components/sample_component/entrypoint.py
+++ src/aml/components/sample_component/entrypoint.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from src.aml.components.sample_component.config import SampleConfig
+from src.aml.components.sample_component.config import ClassificationConfig
 from src.aml.components.sample_component.options import get_config
 from src.common.utils.logger import get_logger
 
@@ -7,7 +7,7 @@ _logger = get_logger(__name__)
 
 
 def main() -> None:
-    config: SampleConfig = get_config()
+    config: ClassificationConfig = get_config()
     _logger.info(f"Running with following config: {config}")
 
 
