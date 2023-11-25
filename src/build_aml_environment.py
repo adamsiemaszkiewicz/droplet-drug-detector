9b5f292692b9432f2db156d57e004393ad51aa49
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-25 12:24:35 +0100

    Move all AML related things into one place

--- src/build_aml_environment.py
+++ src/build_aml_environment.py
@@ -7,8 +7,8 @@ from azure.ai.ml.entities import Environment
 from pydantic import BaseModel
 from typing_extensions import Literal
 
-from src.common.aml.client import get_ml_client
-from src.common.aml.environment import build_environment
+from src.aml.client import get_ml_client
+from src.aml.environment import build_environment
 from src.common.consts.directories import ENVIRONMENTS_DIR
 from src.common.consts.extensions import YAML
 from src.common.settings.base import Settings
