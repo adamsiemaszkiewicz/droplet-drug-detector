c806cf228c7d451ff4ce867d397fd0cf1a6d85a4
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-25 16:48:20 +0100

    Add project specific consts

--- src/common/consts/__init__.py
+++ src/common/consts/__init__.py
@@ -3,3 +3,5 @@ from src.common.consts import azure  # noqa: F401
 from src.common.consts import directories  # noqa: F401
 from src.common.consts import extensions  # noqa: F401
 from src.common.consts import logger  # noqa: F401
+from src.common.consts import machine_learning  # noqa: F401
+from src.common.consts import project  # noqa: F401
