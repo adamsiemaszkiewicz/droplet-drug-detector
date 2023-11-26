bea5906b7f5412ce8bf2e92b9e8c6c9e0bf0e92a
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-26 12:38:04 +0100

    Add folder name & project name consts

--- src/common/consts/project.py
+++ src/common/consts/project.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
+
+from src.common.consts.directories import CONFIGS_DIR
+from src.common.consts.extensions import YAML
+
 PROJECT_NAME: str = "droplet-drug-detector"
 PROJECT_NAME_CLASSIFICATOR: str = "droplet-drug-classificator"
+PROJECT_NAME_REGRESSOR: str = "droplet-drug-regressor"
+
+MODEL_CHECKPOINTS_FOLDER_NAME: str = "checkpoints"
+LOGS_FOLDER_NAME: str = "logs"
+
+DEFAULT_CONFIG_FILE_CLASSIFICATOR: Path = CONFIGS_DIR / f"classificator-default{YAML}"
+DEFAULT_CONFIG_FILE_REGRESSOR: Path = CONFIGS_DIR / f"regressor-default{YAML}"
