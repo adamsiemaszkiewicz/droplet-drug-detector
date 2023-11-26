afdcf87784f096649f6c16baa73f9e1a2cfb2e50
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-26 16:40:44 +0100

    Create plotting utility

--- src/common/consts/machine_learning.py
+++ src/common/consts/machine_learning.py
@@ -7,6 +7,12 @@ STAGE_TRAINING: str = "train"
 STAGE_VALIDATION: str = "val"
 STAGE_TESTING: str = "test"
 
+############
+# PLOTTING #
+############
+
+STAGE_COLORS = {STAGE_TRAINING: "blue", STAGE_VALIDATION: "green", STAGE_TESTING: "red"}
+
 # LABELS #
 
 CLASSIFICTION_INPUT: str = ""
--- src/machine_learning/callbacks/learning_curve.py
+++ src/machine_learning/callbacks/learning_curve.py
@@ -8,13 +8,142 @@ from lightning import Callback, LightningModule, Trainer
 from lightning.pytorch.loggers import MLFlowLogger, WandbLogger
 
 from src.common.consts.extensions import PNG
-from src.common.consts.machine_learning import STAGE_TESTING, STAGE_TRAINING, STAGE_VALIDATION
+from src.common.consts.machine_learning import STAGE_COLORS, STAGE_TESTING, STAGE_TRAINING, STAGE_VALIDATION
 from src.common.utils.logger import get_logger
 
 _logger = get_logger(__name__)
 
 
-stage_colors = {STAGE_TRAINING: "blue", STAGE_VALIDATION: "green", STAGE_TESTING: "red"}
+class LearningCurvePlottingUtility:
+    FIG_SIZE = (12, 10)
+    TITLE_SIZE = 14
+    FONT_SIZE = 10
+    SUBPLOT_ADJUST_BOTTOM = 0.15
+    SUBPLOT_ADJUST_LEFT = 0.15
+    STAGE_COLORS = {STAGE_TRAINING: "blue", STAGE_VALIDATION: "green", STAGE_TESTING: "red"}
+    PROPS_COLOR = "black"
+    ARROW_STYLE = "->"
+    SINGLE_VALUE_MARKER = "o"
+    LABEL_PAD = 10
+
+    def annotate_min_value(self, values: List[float], x_coord: Optional[int] = None) -> None:
+        """
+        Function to annotate the minimum loss value of a stage.
+
+        Args:
+            values (List[float]): List of loss values.
+            x_coord (Optional[int]): The x-coordinate for annotation (for single value).
+        """
+        plt.gca()
+        min_value_idx = np.argmin(values) if x_coord is None else x_coord
+        min_value = values[min_value_idx] if x_coord is None else values[0]
+        plt.annotate(
+            f"Min: {min_value:.2f}",
+            xy=(min_value_idx, min_value),
+            xycoords="data",
+            xytext=(0, -20),
+            textcoords="offset points",
+            arrowprops=dict(facecolor=self.PROPS_COLOR, arrowstyle=self.ARROW_STYLE),
+            horizontalalignment="center",
+            verticalalignment="top",
+            fontsize=self.FONT_SIZE,
+        )
+
+    def annotate_max_value(self, values: List[float], x_coord: Optional[int] = None) -> None:
+        """
+        Function to annotate the maximum value of a stage.
+
+        Args:
+            values (List[float]): List of values.
+            x_coord (Optional[int]): The x-coordinate for annotation (for single value).
+        """
+        plt.gca()
+        max_value_idx = np.argmax(values) if x_coord is None else x_coord
+        max_value = values[max_value_idx] if x_coord is None else values[0]
+        plt.annotate(
+            f"Max: {max_value:.2f}",
+            xy=(max_value_idx, max_value),
+            xycoords="data",
+            xytext=(0, 20),
+            textcoords="offset points",
+            arrowprops=dict(facecolor=self.PROPS_COLOR, arrowstyle=self.ARROW_STYLE),
+            horizontalalignment="center",
+            verticalalignment="bottom",
+            fontsize=self.FONT_SIZE,
+        )
+
+    def annotate_single_value(self, values: List[float], x_coord: Optional[int] = None) -> None:
+        """
+        Function to annotate a single value of a stage.
+
+        Args:
+            values (List[float]): List of values.
+            x_coord (Optional[int]): The x-coordinate for annotation (for single value).
+        """
+        plt.gca()
+        value_idx = x_coord if x_coord is not None else 0
+        value = values[0]
+        plt.annotate(
+            f"Value: {value:.2f}",
+            xy=(value_idx, value),
+            xycoords="data",
+            xytext=(0, 20),
+            textcoords="offset points",
+            arrowprops=dict(facecolor=self.PROPS_COLOR, arrowstyle=self.ARROW_STYLE),
+            horizontalalignment="center",
+            verticalalignment="bottom",
+            fontsize=self.FONT_SIZE,
+        )
+
+    def plot_metric_curve(self, name: str, metric_values: Dict[str, List[float]], total_epochs: int) -> None:
+        """
+        Plot and save the learning curve for a single metric.
+
+        Args:
+            name (str): Name of the metric.
+            metric_values (Dict[str, List[float]]): Dictionary of metric values per stage.
+        """
+        plt.figure(figsize=self.FIG_SIZE)
+
+        min_value = min(min(values) for values in metric_values.values() if values)
+        max_value = max(max(values) for values in metric_values.values() if values)
+
+        for stage, values in metric_values.items():
+            if values:
+                if len(values) == 1:
+                    color = self.STAGE_COLORS.get(stage, "gray")
+                    plt.plot(total_epochs, values[0], marker=self.SINGLE_VALUE_MARKER, label=stage, color=color)
+                    self.annotate_single_value(values=values, x_coord=total_epochs)
+                else:
+                    color = self.STAGE_COLORS.get(stage, "gray")
+                    plt.plot(values, label=stage, color=color)
+                    self.annotate_min_value(values=values)
+                    self.annotate_max_value(values=values)
+
+        # Custom y-axis label and ticks for loss
+        if name.lower() == "loss":
+            plt.yscale("log")
+            plt.ylabel(ylabel="Loss (logarithmic scale)", fontsize=self.FONT_SIZE, labelpad=self.LABEL_PAD)
+
+            y_ticks = np.geomspace(min_value, max_value, 10)
+            plt.yticks(y_ticks, [f"{tick:.1f}" for tick in y_ticks])
+        else:
+            y_ticks = np.linspace(min_value, max_value, 10)
+            plt.yticks(y_ticks, [f"{tick:.1f}" for tick in y_ticks])
+            plt.ylabel(y_label=name, fontsize=self.FONT_SIZE, labelpad=self.LABEL_PAD)
+
+        plt.xlabel("Epoch number", fontsize=self.FONT_SIZE, labelpad=self.LABEL_PAD)
+        x_ticks = list(range(total_epochs + 1))
+        plt.xticks(ticks=x_ticks)
+
+        plt.legend(title=f"average {name} per epoch", fontsize=self.FONT_SIZE)
+
+        plt.grid(True)
+
+        plt.title(f"Learning Curve ({name})", fontsize=self.TITLE_SIZE, pad=16)
+
+        plt.subplots_adjust(bottom=0.15, left=0.15)
+        plt.tight_layout()
 
 
 class LearningCurveCallback(Callback):
@@ -40,6 +169,8 @@ class LearningCurveCallback(Callback):
         self.output_dir = output_dir
         self.log_loss = log_loss
         self.log_metrics = log_metrics
+
+        self.plotting_utility = LearningCurvePlottingUtility()
 
         self.epoch_losses: Dict[str, List[float]] = {STAGE_TRAINING: [], STAGE_VALIDATION: [], STAGE_TESTING: []}
         self.epoch_metrics: Dict[str, Dict[str, List[float]]] = {
@@ -140,11 +271,11 @@ class LearningCurveCallback(Callback):
             if values:
                 if len(values) == 1:
                     marker = "o" if stage == STAGE_TRAINING else "s" if stage == STAGE_VALIDATION else "d"
-                    color = stage_colors.get(stage, "gray")
+                    color = STAGE_COLORS.get(stage, "gray")
                     plt.plot(total_epochs, values[0], marker, label=stage, color=color)
                     self._annotate_single_value(values=values, x_coord=total_epochs)
                 else:
-                    color = stage_colors.get(stage, "gray")
+                    color = STAGE_COLORS.get(stage, "gray")
                     plt.plot(values, label=stage, color=color)
                     self._annotate_min_value(values=values)
                     self._annotate_max_value(values=values)
