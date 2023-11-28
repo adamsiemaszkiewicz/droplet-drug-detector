8cb954eaf329c1c4a1a2f3edd895a37fd51fe02f
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-28 18:47:58 +0100

    Remove default values

--- src/aml/components/classificator_training/data/config.py
+++ src/aml/components/classificator_training/data/config.py
@@ -3,14 +3,13 @@ from pathlib import Path
 
 from pydantic import BaseModel
 
-from src.common.consts.directories import DATA_DIR
 from src.common.utils.logger import get_logger
 
 _logger = get_logger(__name__)
 
 
 class ClassificationDataConfig(BaseModel):
-    dataset_dir: Path = DATA_DIR / "dataset"
-    val_split: float = 0.1
-    test_split: float = 0.1
-    batch_size: int = 32
+    dataset_dir: Path
+    val_split: float
+    test_split: float
+    batch_size: int
