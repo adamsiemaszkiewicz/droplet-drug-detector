0615ca3e1c4457ec5745fa0a2716fa52132af02e
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-28 23:17:41 +0100

    Add confusion matrix dir

--- src/common/consts/project.py
+++ src/common/consts/project.py
@@ -11,6 +11,7 @@ PROJECT_NAME_REGRESSOR: str = "droplet-drug-regressor"
 MODEL_CHECKPOINTS_FOLDER_NAME: str = "checkpoints"
 LOGS_FOLDER_NAME: str = "logs"
 LEARNING_CURVES_FOLDER_NAME: str = "learning_curves"
+CONFUSION_MATRIX_FOLDER_NAME: str = "confusion_matrix"
 
 DEFAULT_CONFIG_FILE_CLASSIFICATOR: Path = CONFIGS_DIR / f"classificator-default{YAML}"
 DEFAULT_CONFIG_FILE_REGRESSOR: Path = CONFIGS_DIR / f"regressor-default{YAML}"
