46ce881a36454034dfa27ca954dc24a66a07a651
Adam Siemaszkiewicz <adamsiemaszkiewicz@gmail.com>
2023-11-28 19:01:32 +0100

    Load data individually for each dataset

--- src/aml/components/classificator_training/data/datamodule.py
+++ src/aml/components/classificator_training/data/datamodule.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+import re
 from collections import Counter
-from copy import deepcopy
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 
 from lightning import LightningDataModule
-from torch.utils.data import DataLoader, Dataset
+from pandas import DataFrame
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
 
 from src.aml.components.classificator_training.data.config import ClassificationDataConfig
 from src.aml.components.classificator_training.data.dataset import DropletDrugClassificationDataset
 from src.aml.components.classificator_training.data.splitters import BaseSplitter
+from src.common.consts.extensions import JPG
 from src.common.utils.logger import get_logger
 from src.common.utils.os import get_cpu_worker_count
 from src.machine_learning.preprocessing.factory import DataPreprocessor
@@ -32,22 +35,71 @@ class ClassificationDataModule(LightningDataModule):
         self.val_dataset: Optional[DropletDrugClassificationDataset] = None
         self.test_dataset: Optional[DropletDrugClassificationDataset] = None
 
+    def _load_full_dataset_dataframe(self) -> DataFrame:
+        def validate_substance_name(name: str) -> str:
+            if name not in DropletDrugClassificationDataset.CLASSES.values():
+                raise ValueError(f"Unknown substance name: {name}")
+            return name
+
+        def validate_concentration(concentration: str) -> float:
+            if not concentration.endswith("mgml"):
+                raise ValueError(f"Unknown substance concentration: {concentration}")
+
+            value_str = re.sub(pattern=r"mgml$", repl="", string=concentration)
+            return float(value_str)
+
+        def validate_lens_zoom(zoom: str) -> float:
+            if not zoom.startswith("x"):
+                raise ValueError(f"Unknown lens zoom: {zoom}")
+            return float(zoom.lstrip("x"))
+
+        sample_subset_dirs = sorted([path for path in self.dataset_dir.iterdir() if path.is_dir()])
+
+        samples = []
+        for subset_dir in sample_subset_dirs:
+            name_split = subset_dir.name.split("_")
+            substance_name = validate_substance_name(name_split[0])
+            substance_idx = list(DropletDrugClassificationDataset.CLASSES.values()).index(substance_name)
+            concentration = validate_concentration(name_split[1])
+            lens_zoom = validate_lens_zoom(name_split[2])
+
+            for image_path in subset_dir.glob(f"*{JPG}"):
+                samples.append([image_path, substance_name, substance_idx, concentration, lens_zoom])
+
+        return DataFrame(
+            samples, columns=["image_path", "substance_name", "substance_idx", "concentration", "lens_zoom"]
+        )
+
     def setup(self, stage: Optional[str] = None) -> None:
-        # Create a full dataset without transforms to split it first
-        full_dataset = DropletDrugClassificationDataset(root_dir=self.dataset_dir)
+        full_dataset_df = self._load_full_dataset_dataframe()
 
-        # Split the dataset into training, validation, and test subsets
-        train_subset, val_subset, test_subset = self.splitter.split(dataset=full_dataset)
+        filtered_dataset_df = full_dataset_df.copy()[full_dataset_df["concentration"] == 1.0]
 
-        # Assign the subset indices to the respective datasets
-        self.train_dataset = self._create_subset_dataset(full_dataset, train_subset.indices)
-        self.val_dataset = self._create_subset_dataset(full_dataset, val_subset.indices)
-        self.test_dataset = self._create_subset_dataset(full_dataset, test_subset.indices)
+        train_df, temp_df = train_test_split(
+            filtered_dataset_df, test_size=self.config.val_split + self.config.test_split
+        )
+        val_df, test_df = train_test_split(temp_df, test_size=self.config.test_split)
 
-        _logger.info(f"Total dataset size: {len(full_dataset)}")
-        _logger.info(f"Training set size: {len(self.train_dataset or [])}")
-        _logger.info(f"Validation set size: {len(self.val_dataset or [])}")
-        _logger.info(f"Test set size: {len(self.test_dataset or [])}")
+        self.train_dataset = DropletDrugClassificationDataset(
+            image_paths=train_df["image_path"].tolist(),
+            labels=train_df["substance_idx"].tolist(),
+            transform=self.preprocessor,
+        )
+        self.val_dataset = DropletDrugClassificationDataset(
+            image_paths=val_df["image_path"].tolist(),
+            labels=val_df["substance_idx"].tolist(),
+            transform=self.preprocessor,
+        )
+        self.test_dataset = DropletDrugClassificationDataset(
+            image_paths=test_df["image_path"].tolist(),
+            labels=test_df["substance_idx"].tolist(),
+            transform=self.preprocessor,
+        )
+
+        _logger.info(f"Total dataset size: {len(filtered_dataset_df)}")
+        _logger.info(f"Training set size: {len(self.train_dataset)}")
+        _logger.info(f"Validation set size: {len(self.val_dataset)}")
+        _logger.info(f"Test set size: {len(self.test_dataset)}")
 
         _logger.info(f"Overall class balance: {self.class_balance}")
         _logger.info(f"Training set class balance: {self.train_class_balance}")
@@ -64,11 +116,6 @@ class ClassificationDataModule(LightningDataModule):
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(dataset=self.test_dataset, batch_size=self.batch_size, num_workers=self.cpu_workers)
-
-    def _create_subset_dataset(self, full_dataset: Dataset, indices: List[int]) -> Dataset:
-        subset_dataset = deepcopy(full_dataset)
-        subset_dataset.samples = [full_dataset.samples[i] for i in indices]
-        return subset_dataset
 
     @property
     def class_balance(self) -> Dict[int, int]:
